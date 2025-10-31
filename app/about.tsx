7bebf818e0f1423e89af3f568896766db3385f77
octaviomds <151770584+octaviomds@users.noreply.github.com>
2025-10-31 07:48:54 +0100

    about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,5 +1,21 @@
-import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
