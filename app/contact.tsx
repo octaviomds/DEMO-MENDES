5ceb471ba66c79a78668e7e02a58127bf9b79503
octaviomds <151770584+octaviomds@users.noreply.github.com>
2025-10-31 08:54:25 +0100

    contact.tsx

--- app/contact.tsx
+++ app/contact.tsx
@@ -11,7 +11,7 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#2a2a2a',
     justifyContent: 'center',
     alignItems: 'center',
   },
